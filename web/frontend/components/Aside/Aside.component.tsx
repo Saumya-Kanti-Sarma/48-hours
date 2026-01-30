c665a7c99eb01b3b402c688720e4087ad1ae5792
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2026-01-30 22:22:44 +0530

    added all features of sidebar

--- web/frontend/components/Aside/Aside.component.tsx
+++ web/frontend/components/Aside/Aside.component.tsx
@@ -8,8 +8,22 @@ import ListWrapper from "@/UI/ListWrapper/ListWrapper.ui"
 
 const Aside = ({ showAside }: asideProps) => {
   return (
-    <aside className={`${showAside ? "left-0" : "-left-full"} absolute
-     w-full p-5 bg-(--black) h-[calc(100vh-71px)] text-white mt-px max-sm:h-[calc(100vh-60px)] overflow-auto transition-all duration-400 ease-in-out`}>
+    <aside
+      className={`
+    absolute w-full p-5 bg-(--black) text-white mt-px
+    h-[calc(100vh-71px)] max-sm:h-[calc(100vh-60px)]
+    overflow-auto transition-all duration-400 ease-in-out
+
+    /* Small screens: slide from left */
+    ${showAside ? "left-0" : "-left-full"}
+    max-[800px]:translate-y-0
+
+    /* ≥800px: slide from top */
+    min-[801px]:left-0
+    ${showAside ? "min-[801px]:translate-y-0" : "min-[801px]:-translate-y-[150%]"}
+  `}
+    >
+
       <div className="pb-5 flex flex-wrap justify-between items-center">
         <SearchBox
           placeHolder="Search for News, Articles and more..."
@@ -36,6 +50,62 @@ const Aside = ({ showAside }: asideProps) => {
         <ListWrapper heading="Self Help" topics={["Best Podcasts of 2025", "Best Books for Software engineers", "Best Book for Engineers", "Are you joking Fynman?", "Napolean Hills"]} />
       </div>
       <hr className="border-(--semi-white) max-[642px]:hidden" />
+
+      <div className="flex justify-between gap-3 align-top flex-wrap pb-5">
+        <ListWrapper
+          heading="Money"
+          topics={[
+            "Nifty 50",
+            "Sensex",
+            "Bank Nifty",
+            "Dow Jones Industrial Average",
+            "S&P 500",
+            "NASDAQ Composite"
+          ]}
+        />
+
+        <ListWrapper
+          heading="Digital Assets"
+          topics={[
+            "Bitcoin (BTC)",
+            "Ethereum (ETH)",
+            "Solana (SOL)",
+            "Central Bank Digital Currencies (CBDC)",
+            "Tokenized Assets",
+            "Web3 Economy"
+          ]}
+        />
+
+        <ListWrapper
+          heading="Business"
+          topics={[
+            "Startup Funding",
+            "Mergers & Acquisitions",
+            "IPO Watch",
+            "Corporate Earnings",
+            "Global Supply Chains",
+            "MSME Growth"
+          ]}
+        />
+
+        <ListWrapper
+          heading="Entrepreneurs"
+          topics={[
+            "Elon Musk",
+            "Mukesh Ambani",
+            "Ratan Tata",
+            "Sam Altman",
+            "Nandan Nilekani",
+            "Women Founders"
+          ]}
+        />
+      </div>
+
+      <hr className="border-(--semi-white) max-[642px]:hidden" />
+      <div>
+        <p className="p-2">© 2026 48-hours. All Rights Reserved. (A project work done by <b>Saumya Sarma</b>)</p>
+        <p className="pb-2">Source Code: <Link href={"/https://github.com/Saumya-Kanti-Sarma/48-hours"} target="_blank" className="underline text-blue-300">Git Hub</Link></p>
+      </div>
     </aside>
   )
 }
